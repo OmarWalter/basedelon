4e6397d7fbd283ad8a6ac2d814cbf6fcb34be865
OmarWalter <starlights0914@gmail.com>
2026-05-02 17:05:15 -0700

    .

--- _next/static/chunks/a3fabffb10d3fbfb.js
+++ _next/static/chunks/a3fabffb10d3fbfb.js
@@ -605,7 +605,7 @@
                   (0, t.jsx)("div", {
                     className: "hidden sm:flex items-center gap-4",
                     children: (0, t.jsx)("a", {
-                      href: "https://app.uniswap.org/swap?chain=mainnet&inputCurrency=NATIVE&outputCurrency=0xComingSoon",
+                      href: "https://app.uniswap.org/swap?chain=mainnet&inputCurrency=NATIVE&outputCurrency=0x5c9e5e02c6f1f9b386fcfc6eeb7da4f33e091c7a",
                       target: "_blank",
                       rel: "noopener noreferrer",
                       className: "manga-btn text-lg py-2",
@@ -764,14 +764,14 @@
                             (0, t.jsx)("code", {
                               className:
                                 "bg-black/5 px-3 py-1 rounded font-mono text-sm",
-                              children: "0xComingSoon...",
+                              children: "0x5c9e5e02c6f1f9b386fcfc6eeb7da4f33e091c7a...",
                             }),
                           ],
                         }),
                         (0, t.jsx)("button", {
                           onClick: () => {
                             navigator.clipboard.writeText(
-                              "0xComingSoon"
+                              "0x5c9e5e02c6f1f9b386fcfc6eeb7da4f33e091c7a"
                             ),
                               a(!0),
                               setTimeout(() => a(!1), 2e3);
@@ -1269,12 +1269,12 @@
       //     //                           className:
       //     //                             "bg-black text-white p-3 rounded font-mono text-sm break-all flex-1 border-2 border-black",
       //     //                           children:
-      //     //                             "0xComingSoon",
+      //     //                             "0x5c9e5e02c6f1f9b386fcfc6eeb7da4f33e091c7a",
       //     //                         }),
       //     //                         (0, t.jsx)("button", {
       //     //                           onClick: () => {
       //     //                             navigator.clipboard.writeText(
-      //     //                               "0xComingSoon"
+      //     //                               "0x5c9e5e02c6f1f9b386fcfc6eeb7da4f33e091c7a"
       //     //                             ),
       //     //                               a(!0),
       //     //                               setTimeout(() => a(!1), 2e3);
@@ -1728,7 +1728,7 @@
                           "flex flex-col sm:flex-row gap-6 pt-6 justify-center lg:justify-start",
                         children: [
                           (0, t.jsx)("a", {
-                            href: "https://app.uniswap.org/swap?chain=mainnet&inputCurrency=NATIVE&outputCurrency=0xComingSoon",
+                            href: "https://app.uniswap.org/swap?chain=mainnet&inputCurrency=NATIVE&outputCurrency=0x5c9e5e02c6f1f9b386fcfc6eeb7da4f33e091c7a",
                             className:
                               "manga-btn bg-black text-white text-2xl py-5 px-10",
                             children: "BUY $BASEDLON",
